67ba082694b3872840a1726481d68b63ac98b4a6
romedu <romedu1@hotmail.com>
2020-01-01 02:20:18 -0400

    Create the errorHandler helper function tests and the folder test file

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,48 @@
-const request = require("supertest"),
-   mongoose = require("mongoose"),
-   app = require("../app");
+const { STATUS_CODES } = require("http"),
+   { errorHandler } = require("../helpers/error");
 
-describe("Folder routes", () => {
-   const baseUrl = "/api/folder";
+describe("Error handler", () => {
+   describe("Call without passing parameters", () => {
+      let errorObject;
 
-   afterAll(() => {
-      mongoose.connection.close();
+      beforeAll(() => {
+         errorObject = errorHandler();
+      })
+
+      it("should return an instance of the error object", () => {
+         expect(errorObject).toBeInstanceOf(Error);
+      });
+
+      it("should return with the status property's value of 500", () => {
+         expect(errorObject.status).toBe(500);
+      });
+
+      it("should return with the message property's value of the status code 500 message", () => {
+         const expectedMessage = STATUS_CODES["500"];
+         expect(errorObject.message).toBe(expectedMessage);
+      });
    });
 
-   describe("Unauthorized requests", () => {
-      describe("Get request", () => {
-         it("should return a status of 401", async done => {
-            const response = await request(app).get(baseUrl);
-            expect(response.status).toBe(401);
-            done();
+   describe("Call passing the first parameter", () => {
+      const testStatus = 200;
+      let errorObject;
+
+      describe("Passing a valid parameter", () => {
+         beforeAll(() => {
+            errorObject = errorHandler(testStatus);
+         })
+
+         it("should return an instance of the error object", () => {
+            expect(errorObject).toBeInstanceOf(Error);
+         });
+
+         it("should return with the status property's value of the same passed", () => {
+            expect(errorObject.status).toBe(testStatus);
+         });
+
+         it("should return with the message property's value of the passed status message", () => {
+            const expectedMessage = STATUS_CODES[testStatus];
+            expect(errorObject.message).toBe(expectedMessage);
          });
       });
    });
