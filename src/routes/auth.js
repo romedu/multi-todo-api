b833bd13adaf0e8028734f6fa32c1fa47c4872ea
romedu <romedu1@hotmail.com>
2020-01-06 17:37:17 -0400

    Create and implement new input validators for the register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router(),
-	helpers = require("../helpers/auth"),
-	{ checkIfToken } = require("../middlewares");
+   helpers = require("../helpers/auth"),
+   { checkIfToken } = require("../middlewares"),
+   { userValidators, confirmValidation } = require("../helpers/validator");
 
-router.post("/register", helpers.register);
+router.post("/register", userValidators, confirmValidation, helpers.register);
 router.post("/login", helpers.login);
 router.get("/verify", checkIfToken, helpers.verifyToken);
 
