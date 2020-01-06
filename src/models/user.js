dd99a1a38650688089bfd7e9046b751a33078ccf
romedu <romedu1@hotmail.com>
2020-01-06 14:26:38 -0400

    Remove a comment from the user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,55 +1,50 @@
 const mongoose = require("mongoose"),
-	bcrypt = require("bcryptjs"),
-	validator = require("../helpers/validator"),
-	userSchema = new mongoose.Schema({
-		username: {
-			type: String,
-			required: [true, "Username is required"],
-			unique: true,
-			minlength: [4, "Only between 4 and 20 characters are allowed"],
-			maxlength: [20, "Only between 4 and 20 characters are allowed"],
-			validate: {
-				validator: validator.alphanumOnly,
-				message: "Only alphanumeric and space characters are allowed"
-			}
-		},
-		// email: {
-		//    type: String,
-		//    required: true,
-		//    unique: true
-		// },
-		password: {
-			type: String,
-			required: [true, "Password is required"],
-			minlength: [8, "Only between 8 and 24 characters are allowed"],
-			maxlength: [24, "Only between 8 and 24 characters are allowed"]
-		},
-		isAdmin: {
-			type: Boolean,
-			default: false
-		}
-	});
+   bcrypt = require("bcryptjs"),
+   validator = require("../helpers/validator"),
+   userSchema = new mongoose.Schema({
+      username: {
+         type: String,
+         required: [true, "Username is required"],
+         unique: true,
+         minlength: [4, "Only between 4 and 20 characters are allowed"],
+         maxlength: [20, "Only between 4 and 20 characters are allowed"],
+         validate: {
+            validator: validator.alphanumOnly,
+            message: "Only alphanumeric and space characters are allowed"
+         }
+      },
+      password: {
+         type: String,
+         required: [true, "Password is required"],
+         minlength: [8, "Only between 8 and 24 characters are allowed"],
+         maxlength: [24, "Only between 8 and 24 characters are allowed"]
+      },
+      isAdmin: {
+         type: Boolean,
+         default: false
+      }
+   });
 
-userSchema.pre("save", async function(next) {
-	try {
-		if (!this.isModified("password")) return next();
-		this.password = await bcrypt.hash(this.password, 11);
-		return next();
-	} catch (error) {
-		error.status = 500;
-		return next(error);
-	}
+userSchema.pre("save", async function (next) {
+   try {
+      if (!this.isModified("password")) return next();
+      this.password = await bcrypt.hash(this.password, 11);
+      return next();
+   } catch (error) {
+      error.status = 500;
+      return next(error);
+   }
 });
 
-userSchema.methods.comparePassword = async function(passedPassword, next) {
-	try {
-		let compareResult = await bcrypt.compare(passedPassword, this.password);
-		if (!compareResult) throw new Error("Incorrect Username/Password");
-		return compareResult;
-	} catch (error) {
-		error.status = 404;
-		return error;
-	}
+userSchema.methods.comparePassword = async function (passedPassword, next) {
+   try {
+      let compareResult = await bcrypt.compare(passedPassword, this.password);
+      if (!compareResult) throw new Error("Incorrect Username/Password");
+      return compareResult;
+   } catch (error) {
+      error.status = 404;
+      return error;
+   }
 };
 
 module.exports = mongoose.model("User", userSchema);
