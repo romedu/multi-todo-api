daad90bc49bdc8903855e39e23a2767450ff7c09
romedu <romedu1@hotmail.com>
2020-01-11 22:48:12 -0400

    Create and implement folder input validation middlewares to make sure it passes the test

--- src/routes/folder.js
+++ src/routes/folder.js
@@ -1,15 +1,18 @@
-const router  = require("express").Router(),
-      helpers = require("../helpers/folder"),
-      {folder} = require("../middlewares");
+const router = require("express").Router(),
+	helpers = require("../helpers/folder"),
+	{ folder } = require("../middlewares"),
+	{ folderValidators, confirmValidation } = require("../helpers/validator");
 
-router.route("/")
-   .get(helpers.find)
-   .post(helpers.create);
+router
+	.route("/")
+	.get(helpers.find)
+	.post(folderValidators, confirmValidation, helpers.create);
 
-router.route("/:id")
-   .all(folder.getCurrentFolder, folder.checkPermission)
-   .get(helpers.findOne)
-   .patch(helpers.update)
-   .delete(folder.ownerPrivileges, helpers.delete);
+router
+	.route("/:id")
+	.all(folder.getCurrentFolder, folder.checkPermission)
+	.get(helpers.findOne)
+	.patch(helpers.update)
+	.delete(folder.ownerPrivileges, helpers.delete);
 
 module.exports = router;
