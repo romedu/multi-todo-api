5c5a2e1559d6ccb0f4dbefe104f74fc602cb3dc0
romedu <romedu1@hotmail.com>
2020-01-26 00:26:17 -0400

    Move the index utilities file to the test root folder

---
+++ test/utilities.js
@@ -0,0 +1,30 @@
+const request = require("supertest"),
+	app = require("../src/app"),
+	dbModels = require("../src/models");
+
+exports.createTestUser = async testUserData => {
+	const registerRouteUrl = "/api/auth/register",
+		{ body: newUserData } = await request(app)
+			.post(registerRouteUrl)
+			.send(testUserData);
+
+	return newUserData;
+};
+
+exports.deleteAllUsers = async () => {
+	await dbModels.User.deleteMany({}).exec();
+};
+
+exports.deleteAllFolders = async () => {
+	await dbModels.Folder.deleteMany({}).exec();
+};
+
+exports.deleteAllTodoLists = async () => {
+	await dbModels.TodoList.deleteMany({}).exec();
+};
+
+exports.deleteAllTodos = async () => {
+	await dbModels.Todo.deleteMany({}).exec();
+};
+
+module.exports = exports;
