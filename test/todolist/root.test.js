b6f63240dcc6bd01e2d8a306aea813430600f82a
romedu <romedu1@hotmail.com>
2020-01-29 23:21:40 -0400

    Add and update todoList root route test cases

--- test/todolist/root.test.js
+++ test/todolist/root.test.js
@@ -1,199 +1,257 @@
 const request = require("supertest"),
-   app = require("../../src/app"),
-   { createTestUser } = require("../utilities");
+	app = require("../../src/app"),
+	{ createTestUser } = require("../utilities");
 
 describe("TodoList routes", () => {
-   describe("/todos", () => {
-      const baseUrl = "/api/todos",
-         todolistObjectSchema = {
-            _id: expect.any(String),
-            name: expect.any(String),
-            todos: expect.any(Array),
-            creator: expect.any(String)
-         };
+	describe("/todos", () => {
+		const baseUrl = "/api/todos",
+			todolistObjectSchema = {
+				_id: expect.any(String),
+				name: expect.any(String),
+				todos: expect.any(Array),
+				creator: expect.any(String)
+			};
 
-      let userData;
+		let userData, authorizationToken;
 
-      beforeAll(async () => {
-         const testUserCredentials = {
-            username: "listTestUsername",
-            password: "listTestPassword"
-         };
+		beforeAll(async () => {
+			const testUserCredentials = {
+					username: "listTestUsername",
+					password: "listTestPassword"
+				},
+				newTestUser = await createTestUser(testUserCredentials);
 
-         userData = await createTestUser(testUserCredentials);
-      });
+			userData = newTestUser;
+			authorizationToken = newTestUser.token;
+		});
 
-      describe("Authorized requests", () => {
-         describe("Get request", () => {
-            let response;
+		describe("Authorized requests", () => {
+			describe("Get request", () => {
+				const paginatedTodoListSchema = {
+					docs: expect.any(Array),
+					total: expect.any(Number),
+					limit: expect.any(Number)
+				};
 
-            beforeAll(async () => {
-               const { token: authorizationToken } = userData;
-               response = await request(app).get(baseUrl).set("Authorization", authorizationToken);
-            });
+				describe("Passing a valid limit query param", () => {
+					const queryParams = { limit: 10 };
 
-            it("should return a status of 200", () => {
-               expect(response.status).toBe(200);
-            });
+					let response;
 
-            it("should return an array of todolists", () => {
-               const { body: todolists } = response;
-               expect(todolists).toBeInstanceOf(Array);
-            })
-         });
+					beforeAll(async () => {
+						response = await request(app)
+							.get(baseUrl)
+							.query(queryParams)
+							.set("Authorization", authorizationToken);
+					});
 
-         describe("Post request", () => {
-            describe("Sending valid data", () => {
-               const testTodoListData = {
-                  name: "testListName"
-               };
+					it("should return a status of 200", () => {
+						expect(response.status).toBe(200);
+					});
 
-               let response;
+					it("should return a paginated todoLsit object", () => {
+						expect(response.body).toEqual(
+							expect.objectContaining(paginatedTodoListSchema)
+						);
+					});
 
-               beforeAll(async () => {
-                  const { token: authorizationToken } = userData;
-                  response = await request(app).post(baseUrl).set("Authorization", authorizationToken).send(testTodoListData);
-               });
+					it("should return with a limit property with the same value as the one passed", () => {
+						expect(response.body.limit).toBe(queryParams.limit);
+					});
+				});
 
-               it("should return a status of 201", () => {
-                  expect(response.status).toBe(201);
-               });
+				describe("Passing an invalid limit query param", () => {
+					const queryParams = { limit: "invalid limit" };
 
-               it("should return a todolist object", () => {
-                  expect(response.body).toEqual(expect.objectContaining(todolistObjectSchema));
-               });
+					let response;
 
-               it("should save the new todolist", async () => {
-                  const { token: authorizationToken } = userData,
-                     { body: newTodoList } = response,
-                     { body: todolistListAfterRequest } = await request(app).get(baseUrl).set("Authorization", authorizationToken),
-                     isNewTodoListSaved = todolistListAfterRequest.some(todolist => todolist.id === newTodoList.id);
+					beforeAll(async () => {
+						response = await request(app)
+							.get(baseUrl)
+							.query(queryParams)
+							.set("Authorization", authorizationToken);
+					});
 
-                  expect(isNewTodoListSaved).toBeTruthy();
-               });
+					it("should return a status of 200", () => {
+						expect(response.status).toBe(200);
+					});
 
-               it("should return a name property with the same value as the one passed", () => {
-                  expect(response.body.name).toBe(testTodoListData.name);
-               });
+					it("should return a paginated todoLsit object", () => {
+						expect(response.body).toEqual(
+							expect.objectContaining(paginatedTodoListSchema)
+						);
+					});
 
-               it("should return a creator property with the same value as the current user id", () => {
-                  expect(response.body.creator).toBe(userData.id);
-               });
+					it("should return with a limit property equal to it's default value", () => {
+						expect(response.body.limit).not.toBe(queryParams.limit);
+					});
+				});
+			});
 
-               it("should not return an errors property", () => {
-                  expect(response.body.errors).toBeUndefined();
-               });
-            });
+			describe("Post request", () => {
+				describe("Sending valid data", () => {
+					const testTodoListData = {
+						name: "testListName"
+					};
 
-            describe("Sending invalid data", () => {
-               describe("Passing none of the inputs", () => {
-                  const testTodoListData = {};
-                  let response;
+					let response;
 
-                  beforeAll(async () => {
-                     const { token: authorizationToken } = userData;
-                     response = await request(app).post(baseUrl).set("Authorization", authorizationToken).send(testTodoListData);
-                  });
+					beforeAll(async () => {
+						response = await request(app)
+							.post(baseUrl)
+							.set("Authorization", authorizationToken)
+							.send(testTodoListData);
+					});
 
-                  it("should return a status of 422", () => {
-                     expect(response.status).toBe(422);
-                  });
+					it("should return a status of 201", () => {
+						expect(response.status).toBe(201);
+					});
 
-                  it("should return an errors property with at least one element", () => {
-                     const errors = response.body.errors || [];
-                     expect(errors.length).toBeGreaterThanOrEqual(1);
-                  });
-               });
+					it("should return a todolist object", () => {
+						expect(response.body).toEqual(
+							expect.objectContaining(todolistObjectSchema)
+						);
+					});
 
-               describe("Sending one of the inputs invalid", () => {
-                  describe("Passing the name input invalid", () => {
-                     const testTodoListData = {
-                        name: "X"
-                     };
+					it("should save the new todolist", async () => {
+						const { body: newTodoList } = response,
+							findTodoListUrl = `${baseUrl}/${newTodoList._id}`,
+							newTodoListQueryResponse = await request(app)
+								.get(findTodoListUrl)
+								.set("Authorization", authorizationToken);
 
-                     let response;
+						expect(newTodoListQueryResponse.status).toEqual(200);
+					});
 
-                     beforeAll(async () => {
-                        const { token: authorizationToken } = userData;
-                        response = await request(app).post(baseUrl).set("Authorization", authorizationToken).send(testTodoListData);
-                     });
+					it("should return a name property with the same value as the one passed", () => {
+						expect(response.body.name).toBe(testTodoListData.name);
+					});
 
-                     it("should return a status of 422", () => {
-                        expect(response.status).toBe(422);
-                     });
+					it("should return a creator property with the same value as the current user id", () => {
+						expect(response.body.creator).toBe(userData.id);
+					});
 
-                     it("should return an errors property with at least one element", () => {
-                        const errors = response.body.errors || [];
-                        expect(errors.length).toBeGreaterThanOrEqual(1);
-                     });
-                  });
-               });
-            });
-         });
+					it("should not return an errors property", () => {
+						expect(response.body.errors).toBeUndefined();
+					});
+				});
 
-         describe("Put request", () => {
-            it("should return a status of 404", async done => {
-               const { token: authorizationToken } = userData,
-                  response = await request(app).put(baseUrl).set("Authorization", authorizationToken);
+				describe("Sending invalid data", () => {
+					describe("Passing none of the inputs", () => {
+						const testTodoListData = {};
+						let response;
 
-               expect(response.status).toBe(404);
-               done();
-            });
-         });
+						beforeAll(async () => {
+							response = await request(app)
+								.post(baseUrl)
+								.set("Authorization", authorizationToken)
+								.send(testTodoListData);
+						});
 
-         describe("Patch request", () => {
-            it("should return a status of 404", async done => {
-               const { token: authorizationToken } = userData,
-                  response = await request(app).patch(baseUrl).set("Authorization", authorizationToken);
+						it("should return a status of 422", () => {
+							expect(response.status).toBe(422);
+						});
 
-               expect(response.status).toBe(404);
-               done();
-            });
-         });
+						it("should return an errors property with at least one element", () => {
+							const errors = response.body.errors || [];
+							expect(errors.length).toBeGreaterThanOrEqual(1);
+						});
+					});
 
-         describe("Delete request", () => {
-            it("should return a status of 404", async done => {
-               const { token: authorizationToken } = userData,
-                  response = await request(app).delete(baseUrl).set("Authorization", authorizationToken);
+					describe("Sending one of the inputs invalid", () => {
+						describe("Passing the name input invalid", () => {
+							const testTodoListData = {
+								name: "X"
+							};
 
-               expect(response.status).toBe(404);
-               done();
-            });
-         });
-      });
+							let response;
 
-      describe("Unauthorized requests", () => {
-         describe("Get request", () => {
-            it("should return a status of 401", async done => {
-               const response = await request(app).get(baseUrl);
-               expect(response.status).toBe(401);
-               done();
-            });
-         });
+							beforeAll(async () => {
+								response = await request(app)
+									.post(baseUrl)
+									.set("Authorization", authorizationToken)
+									.send(testTodoListData);
+							});
 
-         describe("Post request", () => {
-            it("should return a status of 401", async done => {
-               const response = await request(app).post(baseUrl);
-               expect(response.status).toBe(401);
-               done();
-            });
-         });
+							it("should return a status of 422", () => {
+								expect(response.status).toBe(422);
+							});
 
-         describe("Patch request", () => {
-            it("should return a status of 401", async done => {
-               const response = await request(app).patch(baseUrl);
-               expect(response.status).toBe(401);
-               done();
-            });
-         });
+							it("should return an errors property with at least one element", () => {
+								const errors = response.body.errors || [];
+								expect(errors.length).toBeGreaterThanOrEqual(1);
+							});
+						});
+					});
+				});
+			});
 
-         describe("Delete request", () => {
-            it("should return a status of 401", async done => {
-               const response = await request(app).delete(baseUrl);
-               expect(response.status).toBe(401);
-               done();
-            });
-         });
-      });
-   });
+			describe("Put request", () => {
+				it("should return a status of 404", async done => {
+					const response = await request(app)
+						.put(baseUrl)
+						.set("Authorization", authorizationToken);
+
+					expect(response.status).toBe(404);
+					done();
+				});
+			});
+
+			describe("Patch request", () => {
+				it("should return a status of 404", async done => {
+					const response = await request(app)
+						.patch(baseUrl)
+						.set("Authorization", authorizationToken);
+
+					expect(response.status).toBe(404);
+					done();
+				});
+			});
+
+			describe("Delete request", () => {
+				it("should return a status of 404", async done => {
+					const response = await request(app)
+						.delete(baseUrl)
+						.set("Authorization", authorizationToken);
+
+					expect(response.status).toBe(404);
+					done();
+				});
+			});
+		});
+
+		describe("Unauthorized requests", () => {
+			describe("Get request", () => {
+				it("should return a status of 401", async done => {
+					const response = await request(app).get(baseUrl);
+					expect(response.status).toBe(401);
+					done();
+				});
+			});
+
+			describe("Post request", () => {
+				it("should return a status of 401", async done => {
+					const response = await request(app).post(baseUrl);
+					expect(response.status).toBe(401);
+					done();
+				});
+			});
+
+			describe("Patch request", () => {
+				it("should return a status of 401", async done => {
+					const response = await request(app).patch(baseUrl);
+					expect(response.status).toBe(401);
+					done();
+				});
+			});
+
+			describe("Delete request", () => {
+				it("should return a status of 401", async done => {
+					const response = await request(app).delete(baseUrl);
+					expect(response.status).toBe(401);
+					done();
+				});
+			});
+		});
+	});
 });
